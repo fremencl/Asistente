0853d957bfde9d018e6daf2c1add48fda9ec6bdc
Cristian <128819507+fremencl@users.noreply.github.com>
2024-08-22 14:33:39 -0400

    Update App.py
    Cambio radical de enfoque mirando: https://gist.github.com/simonmesmith/cbc0f375aaa9eaadebbecd3ce36bcc9c

--- App.py
+++ App.py
@@ -1,12 +1,15 @@
 import openai
-from openai import OpenAI
 import streamlit as st
 
-# Leer la API Key desde secrets
+# Leer la API Key y el assistant_id desde secrets
 openai_api_key = st.secrets["openai_api_key"]
 assistant_id = st.secrets["assistant_id"]
-openai.organization = "org-XFlSAe5GkaZlemV7G7DZYiNy"
-openai.project = "proj_No911gg9A3eaaNUjxKv3rwcE"
+
+# Crear un cliente de OpenAI
+client = openai.Client(api_key=openai_api_key)
+
+# Recuperar el asistente que quieres usar
+assistant = client.beta.assistants.retrieve(assistant_id)
 
 # Título de la aplicación
 st.title("💬 Asistente Equipo Mantenimiento")
@@ -20,27 +23,29 @@ if "messages" not in st.session_state:
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
-# Cliente de OpenAI con la API Key, organización y proyecto específicos
-client = OpenAI(
-  api_key=openai_api_key,
-  organization="org-XFlSAe5GkaZlemV7G7DZYiNy",  # ID de tu organización
-  project="proj_No911gg9A3eaaNUjxKv3rwcE"  # ID del proyecto por defecto
-)
-
 # Captura la entrada del usuario
 if prompt := st.chat_input():
     # Agrega el mensaje del usuario al estado de la sesión
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
 
-    # Realiza la solicitud de completions
-    response = client.chat.completions.create(
-      model="gpt-4o-mini",  # Especifica el modelo aquí
-      #assistant_id=assistant_id,  # si se confirma que es necesario
-      messages=st.session_state.messages
+    # Crear un nuevo hilo con el mensaje del usuario
+    thread = client.beta.threads.create(
+        messages=[
+            {"role": "user", "content": prompt}
+        ]
     )
 
-    # Obtén la respuesta del asistente y muéstrala en la aplicación
-    msg = response.choices[0].message.content
+    # Ejecutar el hilo con el asistente
+    run = client.beta.threads.runs.create(
+        thread_id=thread.id,
+        assistant_id=assistant.id,
+    )
+
+    # Obtener la respuesta del asistente
+    response = client.beta.threads.messages.list(thread_id=thread.id)
+    msg = response.data[0].content[0].text.value
+
+    # Agregar la respuesta al estado de la sesión
     st.session_state.messages.append({"role": "assistant", "content": msg})
     st.chat_message("assistant").write(msg)
